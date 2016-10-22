e60c80b2048a532a4ae0e820c74db116a531d75e
nnt1054 <ntoledo@berkeley.edu>
2016-10-22 01:07:43 -0700

    New Website
    all new ntoledo website stuff

---
+++ ntoledo.js
@@ -0,0 +1,8 @@
+/*global $, jQuery, alert*/
+$(document).ready(function () {
+    "use strict";
+    var nav = $("#nav");
+    $("#menu-icon").click(function () {
+        $("#navbox").toggleClass("open");
+    });
+});
