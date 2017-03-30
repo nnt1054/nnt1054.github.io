79a9e902f2be64fbee5220b32f3779a104e9acd9
Neil Toledo <ntoledo@berkeley.edu>
2017-03-29 20:06:04 -0700

    started website update

--- ntoledo.js
+++
@@ -1,8 +0,0 @@
-/*global $, jQuery, alert*/
-$(document).ready(function () {
-    "use strict";
-    var nav = $("#nav");
-    $("#menu-icon").click(function () {
-        $("#navbox").toggleClass("open");
-    });
-});
