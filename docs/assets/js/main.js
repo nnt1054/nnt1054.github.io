63a7ec1ac8343c8f4710b9133cd99ad968c28ad1
Neil Toledo <ntoledo@berkeley.edu>
2019-01-30 05:02:14 -0800

    fixed dropdown issue and clicking elsewhere to collapse

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,3 +1,9 @@
-$('.nav-container').on('touchend', function() {
+$('.nav-container').on('click', function() {
 	$('.nav-container').toggleClass('show');
 })
+
+$('body').on('click', function(event) {
+	if (!$(event.target).hasClass('nav-container')) {
+		$('.nav-container').removeClass('show');
+	}
+});
